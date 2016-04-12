1513140a9c5c32760f2a1afe6d3c42185a303877
Marko Markovic <fatdzo@gmail.com>
2016-04-12 11:10:16 -0400

    added favicon; styling changes to the apps page; refactoring;

---
+++ Scripts/globalvariables.js
@@ -0,0 +1,12 @@
+﻿var LANDING_PAGE = 1;
+var APPS_PAGE = 2;
+var RESUME_PAGE = 3;
+var MUSIC_PAGE = 4;
+var CONTACT_PAGE = 5;
+
+var email;
+
+var cemu = 'markomarks';
+var ende = '.com';
+var ka = '@';
+var ko = 'marko';
