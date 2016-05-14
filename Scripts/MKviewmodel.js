79536526a6248a925ece3b6c1eea0e804f6c0908
Marko Markovic <fatdzo@gmail.com>
2016-05-14 17:15:08 -0400

    MK: player death, wining the game, leveling up, reseting hp; resistance calculations

--- Scripts/MKviewmodel.js
+++ Scripts/MKviewmodel.js
@@ -46,7 +46,7 @@ mmviewmodel.PlayerViewModel = function () {
     self.Name = "Unknown King";
     self.Level = 1;
     self.EXP = 0;
-    self.PointsToSpend = 3;
+    self.PointsToSpend = 10;
     self.Wins = 0;
     self.Losses = 0;
     self.HP = 20;
@@ -61,10 +61,46 @@ mmviewmodel.PlayerViewModel = function () {
     self.WATER = 0;
     self.PlayerCards = [];
 
+    self.MaxEXP = 9900;
+    self.NextLevelEXP = 1000;
+
+    self.calculateNextLevelEXP = function () {
+        var result = self.NextLevelEXP + self.Level * 1000;
+        return result;
+    };
+
+    self.killExpValue = function () {
+        var result = self.Level * 1500;
+        return result;
+    };
+
+    self.playerDied = function () {
+        self.Losses += 1;
+    };
+
+    self.playerWins = function (exp) {
+        self.EXP += exp;
+        self.Wins += 1;
+        if (self.EXP >= self.NextLevelEXP) {
+            self.levelUp();
+        }
+    };
+
+    self.isAlive = function () {
+        if (self.CurrentHP <= 0) {
+            return false;
+        }
+        return true;
+    };
+
+    self.resetHP = function () {
+        self.CurrentHP = self.HP;
+    };
+
     self.calculatePlayerDamageType = function (cardtype) {
         var calcdmg = 0;
-        for (var i = 0; i < self.PlayerCards.length; i++){
-            if (self.PlayerCards[i].OpposingCardIndex == -1 && self.PlayerCards[i].IsSelected && self.PlayerCards[i].Status == mmviewmodel.CardStatusEnum.OnTable && self.PlayerCards[i].TYPE == cardtype){
+        for (var i = 0; i < self.PlayerCards.length; i++) {
+            if (self.PlayerCards[i].OpposingCardIndex == -1 && self.PlayerCards[i].IsSelected && self.PlayerCards[i].Status == mmviewmodel.CardStatusEnum.OnTable && self.PlayerCards[i].TYPE == cardtype) {
                 calcdmg += self.PlayerCards[i].DMG;
             }
         }
@@ -83,7 +119,7 @@ mmviewmodel.PlayerViewModel = function () {
             self.STR += newStr;
             self.PointsToSpend -= newStr;
         }
-        else{
+        else {
             self.STR += 1;
             self.PointsToSpend -= 1;
         }
@@ -95,7 +131,7 @@ mmviewmodel.PlayerViewModel = function () {
             self.DEX += newDex;
             self.PointsToSpend -= newDex;
         }
-        else{
+        else {
             self.DEX += 1;
             self.PointsToSpend -= 1;
         }
@@ -130,6 +166,7 @@ mmviewmodel.PlayerViewModel = function () {
 
     self.calculateHP = function () {
         self.HP = 20 + 2 * self.CON;
+        self.resetHP();
     };
 
     self.calculateResistances = function () {
@@ -140,29 +177,37 @@ mmviewmodel.PlayerViewModel = function () {
     };
 
     self.calculateEarth = function () {
-        return self.STR + self.MAG;
+        return Math.round((self.CON + self.MAG) / 2);
     };
 
     self.calculateFire = function () {
-        return self.STR + self.MAG;
+        return Math.round((self.STR + self.MAG)/2);
     };
 
     self.calculateWind = function () {
-        return self.DEX + self.MAG;
+        return self.MAG;
     };
 
     self.calculateWater = function () {
-        return self.DEX + self.CON;
+        return Math.round((self.DEX + self.MAG)/2);
     };
 
-    self.levelUp = function (addStr, addDef, addMag, addCon) {
+    self.levelUp = function () {
+        self.Level += 1;
+        self.PointsToSpend = 2;
+        self.resetHP();
+        self.NextLevelEXP = self.calculateNextLevelEXP();
+        alert("Congratulations, you are now level " + self.Level);
+    };
+
+    /*self.levelUp = function (addStr, addDef, addMag, addCon) {
         self.Level += 1;
         self.STR += addStr;
         self.DEX += addDef;
         self.MAG += addMag;
         self.CON += addCon;
         self.calculateResistances();
-    };
+    };*/
 };
 
 mmviewmodel.numberOfCardsInHand = function (playercards) {
@@ -212,9 +257,19 @@ mmviewmodel.GameViewModel = function () {
     self.GameInProgress = false;
     self.CurrentPlayerCardIndex = -1;
     self.GameStart = function () {
-        self.Player2.Name = "Enemy king";
-        self.Turn = 1;
         self.GameInProgress = true;
+        self.Turn = 1;
+        self.CardsPlayed = 0;
+        self.CardsDrawn = 0;
+        self.NumberOfAttacks = 0;
+        self.CurrentPlayerCardIndex = -1;
+
+        self.Player1.resetHP();
+
+        self.Player2 = new mmviewmodel.PlayerViewModel();
+        self.Player2.Id = PLAYER_2;
+        self.Player2.Name = "Enemy king";
+        
         var playerStart = Math.floor(1 + (Math.random() * 2));
         self.CurrentPlayer = playerStart;
         self.Player1.PlayerCards = [];
@@ -227,6 +282,23 @@ mmviewmodel.GameViewModel = function () {
         mmgamelogic.renderCards(self.Player1.PlayerCards, false);
         mmgamelogic.renderCards(self.Player2.PlayerCards, true);
     };
+    
+    self.endGame = function () {
+        self.GameInProgress = false;
+        stage.removeAllChildren();
+
+        if (!self.Player1.isAlive()) {
+
+            self.Player1 = new mmviewmodel.PlayerViewModel();
+            self.Player1.Id = PLAYER_1;
+
+            alert("You lost! Better luck next time!");
+        }
+        else {
+            alert("You won! The next king awaits your challenge!");
+        }
+    };
+
     self.canStartGame = function () {
         if (!self.Player1.canSpendPoints() && !self.GameInProgress) {
             return false;
@@ -269,18 +341,18 @@ mmviewmodel.GameViewModel = function () {
             return true;
         }
         return false;
-    }
+    };
 
     self.hasMaxCardsDrawn = function () {
         var cardsInHand = mmviewmodel.numberOfCardsInHand(self.getCurrentPlayerCards());
-        
+
         if (cardsInHand < max_num_of_cards_in_hand && self.CardsDrawn < max_cards_draw && self.GameInProgress) {
             return false;
         }
         return true;
-    }
+    };
 
-    self.DrawCard = function () {
+    self.playerDrawsCard = function () {
         var cardsInHand = mmviewmodel.numberOfCardsInHand(self.getCurrentPlayerCards());
         if (cardsInHand < max_num_of_cards_in_hand && self.CardsDrawn < max_cards_draw) {
             var orientationcoef = 1;
@@ -304,7 +376,7 @@ mmviewmodel.GameViewModel = function () {
 
             self.CardsDrawn += 1;
         }
-    }
+    };
     self.attack = function () {
         if (self.NumberOfAttacks < max_number_of_attacks) {
             //Attacking cards
@@ -320,10 +392,10 @@ mmviewmodel.GameViewModel = function () {
                         currentplayerlostcards = true;
                         self.getCurrentPlayerCards()[i].Status = mmviewmodel.CardStatusEnum.GraveYard;
                     }
-                    
+
                     if (self.getOpposingPlayer().PlayerCards[self.getCurrentPlayerCards()[i].OpposingCardIndex].DEF <= 0) {
                         self.getOpposingPlayer().PlayerCards[self.getCurrentPlayerCards()[i].OpposingCardIndex].Status = mmviewmodel.CardStatusEnum.GraveYard;
-                        
+
                         mmgamelogic.moveCardsToTheLeftAnimation(self.getOpposingPlayer().PlayerCards, mmgamelogic.calculateCardYValForPlayer(self.getOpposingPlayer().Id, mmviewmodel.CardStatusEnum.OnTable), mmviewmodel.CardStatusEnum.OnTable);
                     }
                 }
@@ -333,7 +405,7 @@ mmviewmodel.GameViewModel = function () {
                 mmgamelogic.moveCardsToTheLeftAnimation(self.getCurrentPlayerCards(), mmgamelogic.calculateCardYValForPlayer(self.CurrentPlayer, mmviewmodel.CardStatusEnum.OnTable), mmviewmodel.CardStatusEnum.OnTable);
             }
 
-            
+
 
             //Attacking player
             var damageToOpposingPlayer = 0;
@@ -366,6 +438,19 @@ mmviewmodel.GameViewModel = function () {
             damageToOpposingPlayer = totfireDamage + totearthDamage + totwaterDamage + totwindDamage;
             console.log("F- " + totfireDamage + ";E- " + totearthDamage + ";W- " + totwaterDamage + ";WI- " + totwindDamage);
             self.getOpposingPlayer().CurrentHP -= damageToOpposingPlayer;
+
+            if (!self.getOpposingPlayer().isAlive()) {
+                if (self.CurrentPlayer == PLAYER_1) {
+                    self.getCurrentPlayer().playerWins(self.getOpposingPlayer().killExpValue());
+                }
+                self.endGame();
+            }
+
+            if (!self.getCurrentPlayer().isAlive()) {
+                self.getCurrentPlayer().playerDied();
+                self.endGame();
+            }
+
             self.NumberOfAttacks += 1;
 
             mmgamelogic.deselectPlayerCardsOnTable(self.getCurrentPlayerCards());
@@ -390,6 +475,8 @@ mmviewmodel.GameViewModel = function () {
 
         mmgamelogic.deselectPlayerCardsOnTable(self.getCurrentPlayerCards());
         mmgamelogic.deselectPlayerCardsOnTable(self.getOpposingPlayer().PlayerCards);
+
+        self.playerDrawsCard();
     };
 
     self.goToNextTurn = function () {
